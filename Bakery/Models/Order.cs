7f2c3d81a2826e9f5d2aad6cc83601285b4f9536
AlyxMoon <allisterkmoon@gmail.com>
2021-07-23 11:08:16 -0700

    fill out Order class

--- Bakery/Models/Order.cs
+++ Bakery/Models/Order.cs
@@ -1,8 +1,18 @@
+using System;
 
 namespace Bakery.Models
 {
   public class Order
   {
-    
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; }
+
+    public Order (string title = "", string description = "")
+    {
+      Title = title;
+      Description = description;
+      Date = DateTime.Now;
+    }
   }
 }
