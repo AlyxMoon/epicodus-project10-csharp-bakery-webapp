92b7d6b01b540477670ee27def77e62696056a5a
AlyxMoon <allisterkmoon@gmail.com>
2021-07-23 11:08:10 -0700

    add tests for Order class

--- Bakery.Tests/Models.Tests/Order.Tests.cs
+++ Bakery.Tests/Models.Tests/Order.Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
 
@@ -9,7 +10,22 @@ namespace Bakery.Tests
     [TestMethod]
     public void Constructor_InitializesWithCorrectValues ()
     {
-      
+      Order order = new();
+
+      Assert.AreEqual("", order.Title);
+      Assert.AreEqual("", order.Description);
+      // give a little leeway for date check
+      Assert.IsTrue(order.Date >= DateTime.Now.AddSeconds(-5));
+      Assert.IsTrue(order.Date <= DateTime.Now.AddSeconds(5));
+    }
+
+    [TestMethod]
+    public void Constructor_InitializesWithCorrectValues_SetsToValuesProvided ()
+    {
+      Order order = new("Test Order", "Test Description");
+
+      Assert.AreEqual("Test Order", order.Title);
+      Assert.AreEqual("Test Description", order.Description);
     }
   }
 }
