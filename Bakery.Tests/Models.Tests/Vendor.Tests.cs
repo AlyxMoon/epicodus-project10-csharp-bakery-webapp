b349097b7495ca51f69d0a7e79d7aea971762cdd
AlyxMoon <allisterkmoon@gmail.com>
2021-07-23 11:50:59 -0700

    add tests for initializing vendor

---
+++ Bakery.Tests/Models.Tests/Vendor.Tests.cs
@@ -0,0 +1,46 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose ()
+    {
+      Vendor.ResetIdCount();
+    }
+
+    [TestMethod]
+    public void Constructor_InitializesWithCorrectValues ()
+    {
+      Vendor vendor = new();
+
+      Assert.AreEqual("", vendor.Name);
+      Assert.AreEqual("", vendor.Description);
+      Assert.AreEqual(0, vendor.Orders.Count);
+    }
+
+    [TestMethod]
+    public void Constructor_InitializesWithCorrectValues_SetsToValuesProvided ()
+    {
+      Vendor vendor = new("Test Order", "Test Description");
+
+      Assert.AreEqual("Test Order", vendor.Name);
+      Assert.AreEqual("Test Description", vendor.Description);
+    }
+
+    [TestMethod]
+    public void Constructor_ShouldHaveAutoIncrementingId ()
+    {
+      Vendor vendor1 = new();
+      Vendor vendor2 = new();
+      Vendor vendor3 = new();
+
+      Assert.AreEqual(1, vendor1.Id);
+      Assert.AreEqual(2, vendor2.Id);
+      Assert.AreEqual(3, vendor3.Id);
+    }
+  }
+}
