3dd1fafe59e7677cbedd2a0c5f3cfcb818462972
AlyxMoon <allisterkmoon@gmail.com>
2021-07-23 11:51:09 -0700

    add vendor class in with constructor logic

--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -1,8 +1,26 @@
+using System.Collections.Generic;
 
 namespace Bakery.Models
 {
   public class Vendor
   {
-    
+    private static int NextId = 1;
+    public int Id { get; private set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
+
+    public static void ResetIdCount ()
+    {
+      NextId = 1;
+    }
+
+    public Vendor (string name = "", string description = "")
+    {
+      Name = name;
+      Description = description;
+      Orders = new();
+      Id = NextId++;
+    }
   }
 }
